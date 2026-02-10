65bb3fa25bbff7f11b6667f276219ef93407b753
stefkaspiegel@icloud.com <stefkaspiegel@icloud.com>
2026-02-10 12:06:28 +0100

    Initial Commit

---
+++ MellowHabits/MellowHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MellowHabitsApp.swift
+//  MellowHabits
+//
+//  Created by Stefka Spiegel on 10.02.26.
+//
+
+import SwiftUI
+
+@main
+struct MellowHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
